af4cf93a5b9c912b21c0e50b6ad5104c3b0cffbd
PeterZ Li <1846136440@qq.com>
2021-04-18 11:50:56 +0800

    frame

---
+++ CODE_Cpp/algorithm/PTA/PAT (Basic Level) Practice/1001_(3n+1)guess.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+class Step
+{
+    private:
+    int num;
+    public:
+    Step(int n):num(n)
+    {
+        
+    }
+    ~Step()
+    {
+
+    }
+    bool parity(int a)
+    {
+
+    }
+    int discount(int b)
+    {
+
+    }
+};
+
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    Step st(n);
+}
