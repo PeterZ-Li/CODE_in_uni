34d6dd998830a6167e0c72409356c0db0cd12aba
PeterZ Li <1846136440@qq.com>
2021-04-18 10:17:15 +0800

    frame

--- CODE_Cpp/homework/SuperPrime-HW2.cpp
+++ CODE_Cpp/homework/SuperPrime-HW2.cpp
@@ -5,16 +5,12 @@ private:
   int num;
 public:
   Nature():num(0){
-    std::cout << "Default Create Nature as " << num << std::endl;
   }
   Nature(int n):num(n) {
-    std::cout << "Create Nature as " << num << std::endl;
   }
   Nature(const Nature &nat):num(nat.num){
-    std::cout << "Copy Create Nature as " << num << std::endl;
   }
   ~Nature() {
-    std::cout << "Destroy Nature as " << num << std::endl;
   }
 };
 class SuperPrime {
@@ -22,16 +18,12 @@ private:
   std::vector<Nature> natures;
 public:
   SuperPrime(int a, int b) {
-    std::cout << "Create SuperPrime from " << a << " to " << b << std::endl;
     for(int i = a; i < b; i++) {
       Nature nat(i);
-      std::cout << "HAHA" << std::endl;
       natures.push_back(nat);
-      std::cout << "DDDDD" << std::endl;
 	}
   }
   ~SuperPrime() {
-    std::cout << "Destroy SuperPrime " << std::endl;
   }
   
   Nature max() {
@@ -48,10 +40,8 @@ public:
   }
 };
 int main() {
-  SuperPrime sp(10, 13);
+  SuperPrime sp(100，999);
   Nature n = sp.max(); 
-  std::cout << "��󳬼�������" ;
-  n.show();
-  
+  n.show();  
   return 0;
 } 
