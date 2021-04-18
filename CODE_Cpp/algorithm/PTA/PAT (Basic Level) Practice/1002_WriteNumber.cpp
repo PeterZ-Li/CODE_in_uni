18a55644ea4e8b4eb10bb4bc54c87cb162848bc0
PeterZ Li <1846136440@qq.com>
2021-04-18 14:14:30 +0800

    frame

---
+++ CODE_Cpp/algorithm/PTA/PAT (Basic Level) Practice/1002_WriteNumber.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+class WriteNumber
+{
+    private:
+    string str;
+    public:
+    WriteNumber(string str1):str(str1)
+    {
+    }
+    ~WriteNumber()
+    {
+    }
+    int sum(string s)
+    {
+
+    }
+    void print(int n)
+    {
+
+    }
+};
+
+int main()
+{
+    string s;
+    cin>>s;
+    WriteNumber wn(s);
+}
