39af54e5989b7ba3e3f6b5c36c0e8afb08695928
PeterZ Li <1846136440@qq.com>
2021-04-18 14:41:15 +0800

    add note 1

--- CODE_Cpp/algorithm/PTA/PAT (Basic Level) Practice/1002_WriteNumber.cpp
+++ CODE_Cpp/algorithm/PTA/PAT (Basic Level) Practice/1002_WriteNumber.cpp
@@ -16,23 +16,23 @@ class WriteNumber
     int sum(string s)
     {
         int sum=0;
-        for(int i=0;i<(int)s.size();i++)
+        for(int i=0;i<(int)s.size();i++)//size()返回无符号数，要强制类型转换
         {
-            sum+=stoi(s.substr(i,1));
+            sum+=stoi(s.substr(i,1));//转化为int，但为什么不能是 stoi(s[i])？
         }
         return sum;
     }
     void print(int n)
     {
-        string piny[]={"ling","yi","er","san","si","wu","liu","qi","ba","jiu"};
-        string ssum=to_string(n);
+        string piny[]={"ling","yi","er","san","si","wu","liu","qi","ba","jiu"};//利用数组，可以不用switch，且string是char*，指向字符串
+        string ssum=to_string(n);//数字转换成string
         for(int i=0;i<(int)ssum.size();i++)
         {
             if(i!=0)
             {
                 cout<<" ";
             }
-            cout<<piny[ssum[i]-'0'];
+            cout<<piny[ssum[i]-'0'];//'0'是ASCII的数字最低，可将字符串型数字与之相减得到数字
         }
     }
 };
