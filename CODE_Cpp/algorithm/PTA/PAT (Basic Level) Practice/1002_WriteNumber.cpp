538296348b8a275372b96eda608bdfe00d73722c
PeterZ Li <1846136440@qq.com>
2021-04-18 14:37:01 +0800

    finsh with incomprehension

--- CODE_Cpp/algorithm/PTA/PAT (Basic Level) Practice/1002_WriteNumber.cpp
+++ CODE_Cpp/algorithm/PTA/PAT (Basic Level) Practice/1002_WriteNumber.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 class WriteNumber
 {
-    private:
-    string str;
+    //private:
+    //string str;
     public:
-    WriteNumber(string str1):str(str1)
+    WriteNumber()
     {
     }
     ~WriteNumber()
@@ -15,17 +15,35 @@ class WriteNumber
     }
     int sum(string s)
     {
-
+        int sum=0;
+        for(int i=0;i<(int)s.size();i++)
+        {
+            sum+=stoi(s.substr(i,1));
+        }
+        return sum;
     }
     void print(int n)
     {
-
+        string piny[]={"ling","yi","er","san","si","wu","liu","qi","ba","jiu"};
+        string ssum=to_string(n);
+        for(int i=0;i<(int)ssum.size();i++)
+        {
+            if(i!=0)
+            {
+                cout<<" ";
+            }
+            cout<<piny[ssum[i]-'0'];
+        }
     }
 };
 
 int main()
 {
     string s;
+    int n;
     cin>>s;
-    WriteNumber wn(s);
+    WriteNumber wn;
+    n=wn.sum(s);
+    wn.print(n);
+    return 0;
 }
