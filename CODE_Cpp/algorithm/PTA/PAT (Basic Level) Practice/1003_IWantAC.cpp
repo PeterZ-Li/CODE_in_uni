15d11c2bbde88d94388ebe1ef1379172387bdfa3
PeterZ Li <1846136440@qq.com>
2021-04-20 20:27:05 +0800

    framework

---
+++ CODE_Cpp/algorithm/PTA/PAT (Basic Level) Practice/1003_IWantAC.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Character
+{
+    private:
+    string str;
+    public:
+    Character(string s):str(s)
+    {
+
+    }
+    void isPTA()
+    {
+
+    }
+};
+class RightAnswer
+{
+    private:
+    int fri,sec,tri;
+    public:
+    RightAnswer()
+    {
+        string s;
+        cin >> s;
+        Character cha(s);
+    }
+    bool second(int b)
+    {
+
+    }
+    bool compare(int a,int c)
+    {
+
+    }
+    bool calculate(int a,int b,int c)
+    {
+
+    }
+};
+
+int main()
+{
+    int n;
+    cin>>n;
+    while (n--)
+    {
+        RightAnswer AC();
+    }
+}
