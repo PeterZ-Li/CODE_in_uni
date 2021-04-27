0fc307beffe762c6768a31c6c5e239aa5c6bb84c
PeterZ Li <1846136440@qq.com>
2021-04-27 20:18:36 +0800

    framework

--- CODE_Cpp/Cpp_SINGLE/algorithm/PTA/PAT (Basic Level) Practice/1006_NewInt.cpp
+++ CODE_Cpp/Cpp_SINGLE/algorithm/PTA/PAT (Basic Level) Practice/1006_NewInt.cpp
@@ -1,8 +1,20 @@
+#include<iostream>
+
+class NewInt
+{
+    private:
+    int num;
+    public:
+    NewInt(int n):num(n)
+    {
+    }
+    void show();
+};
 int main()
 {
     int n;
     std::cin>>n;
-    class NewInt(n);
-    n.show();
+    NewInt ni(n);
+    ni.show();
     return 0;
 }
