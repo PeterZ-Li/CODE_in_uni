f50368c188c560bea85109a0df60bfb736b8d14a
PeterZ Li <1846136440@qq.com>
2021-04-27 18:32:13 +0800

    solve the problem, give the value first

--- CODE_Cpp/Cpp_SINGLE/test/DynamicArray.cpp
+++ CODE_Cpp/Cpp_SINGLE/test/DynamicArray.cpp
@@ -4,8 +4,8 @@
 int main()
 {
     int n;
-    int *p=new int(n);//use () but not []? *p始终指向第0个元素
-    std::cin>>n;
+    std::cin>>n;//give the value to n first
+    int *p=new int[n];//use () but not []? *p始终指向第0个元素
     for(int i=0;i<n;i++)
     {
         std::cin>>p[i];
