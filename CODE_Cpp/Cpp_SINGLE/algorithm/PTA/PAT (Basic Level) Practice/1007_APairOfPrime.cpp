30faf9fef5ca84f0fbc502dfaeb652902e27350b
PeterZ Li <1846136440@qq.com>
2021-04-27 21:09:02 +0800

    framework, still need some knowledge

--- CODE_Cpp/Cpp_SINGLE/algorithm/PTA/PAT (Basic Level) Practice/1007_APairOfPrime.cpp
+++ CODE_Cpp/Cpp_SINGLE/algorithm/PTA/PAT (Basic Level) Practice/1007_APairOfPrime.cpp
@@ -1,3 +1,43 @@
+#include<iostream>
+#include<vector>
+
+class Guess
+{
+    private:
+    int N;
+    public:
+    Guess(int n):N(n)
+    {
+    }
+    void tell()
+    {
+        Prime pr(N);
+        //use a vector to push_back, and then iterate it to find pairs of prime, at the meantime ++count
+    }
+};
+class Prime:public Nature
+{
+    private:
+    std::vector<int> Pr;
+    public:
+    Prime(int n)//to get the vector
+    {
+        
+    }
+};
+class Nature
+{
+    private:
+    int num;
+    public:
+    Nature(int n):num(n)//get the scale of the nature
+    {
+    }
+    bool isPrime()//judge the nature
+    {
+        
+    }
+};
 int main()
 {
     int N;
