c443ca4365583a5256bd8232079e5baf51bae4d7
PeterZ Li <1846136440@qq.com>
2021-05-02 15:11:42 +0800

    version 1, output incorrect

--- CODE_Cpp/Cpp_SINGLE/algorithm/PTA/PAT (Basic Level) Practice/1007_APairOfPrime.cpp
+++ CODE_Cpp/Cpp_SINGLE/algorithm/PTA/PAT (Basic Level) Practice/1007_APairOfPrime.cpp
@@ -1,6 +1,27 @@
 #include<iostream>
 #include<vector>
 
+class Nature//mind the order of the class, especially you need to use the class in other class
+{
+    private:
+    int num;
+    friend class Guess;
+    public:
+    Nature(int n):num(n)//get the scale of the nature. error: no the scale of nature, but just the nature.
+    {
+    }
+    bool isPrime()//judge the nature
+    {
+    if(num == 1 || num == 0)
+    return false;
+    for(int i = 2; i <= num; i++)
+    {
+        if(num % i == 0)
+        return false;
+    }
+    return true;
+    }
+};
 class Guess
 {
     private:
@@ -11,33 +32,66 @@ class Guess
     }
     void tell()
     {
-        Prime pr(N);
-        //use a vector to push_back, and then iterate it to find pairs of prime, at the meantime ++count
+        std::vector<Nature> Pr;// get the scale of nature, get the prime vector
+        for(int i=2;i<N;i++)
+        {
+            Nature nat(i);
+            if(nat.isPrime())
+            {
+                Pr.push_back(nat);
+            }
+        }
+        //and then iterate it to find pairs of prime, at the meantime ++count
+        int count=0;
+        for(int i=0;i<(int)Pr.size();i++)
+        {
+            if((Pr[i+1].num-Pr[i].num)==2)
+            {
+                count++;
+            }
+        }
+        std::cout<<count;
     }
 };
-class Prime:public Nature
+/*class Prime:public Nature
 {
     private:
-    std::vector<int> Pr;
+    std::vector<Nature> Pr;
+    friend class Guess;
     public:
-    Prime(int n)//to get the vector
+    Prime(int n):Nature(n)//to get the vector
     {
-        
+        for(int i=2;i<n;i++)
+        {
+            Nature nat(i);
+            if(nat.isPrime())
+            {
+                Pr.push_back(nat);
+            }
+        }
     }
-};
-class Nature
+};*/
+/*class Nature
 {
     private:
     int num;
+    friend class Guess;
     public:
-    Nature(int n):num(n)//get the scale of the nature
+    Nature(int n):num(n)//get the scale of the nature. error: no the scale of nature, but just the nature.
     {
     }
     bool isPrime()//judge the nature
     {
-        
+    if(num == 1 || num == 0)
+    return false;
+    for(int i = 2; i <= num; i++)
+    {
+        if(num % i == 0)
+        return false;
     }
-};
+    return true;
+    }
+};*/
 int main()
 {
     int N;
