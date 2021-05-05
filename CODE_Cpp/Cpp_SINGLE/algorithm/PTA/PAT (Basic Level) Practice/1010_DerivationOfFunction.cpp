ee7080d46f6efda56cf4decac8bcf652ae861919
PeterZ Li <1846136440@qq.com>
2021-05-05 16:48:07 +0800

    version 1, the output have sth wrong

--- CODE_Cpp/Cpp_SINGLE/algorithm/PTA/PAT (Basic Level) Practice/1010_DerivationOfFunction.cpp
+++ CODE_Cpp/Cpp_SINGLE/algorithm/PTA/PAT (Basic Level) Practice/1010_DerivationOfFunction.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+//#include<sstream> for a single element in the string, we can not let it as a stream
 #include<vector>
 
 class Derivation
@@ -10,11 +11,29 @@ class Derivation
     public:
     Derivation(std::string pol):polynomial(pol)//get the coefficient
     {
-
+        for(int it=0;it<(int)polynomial.size();it++)
+        {
+            int i;
+            if(polynomial[it]!=' ')
+            {
+                i=polynomial[it]-'0';
+                coefficient.push_back(i);
+            }
+        }
     }
     void derivate()//multiply the variables in vector and output with blank space
     {
-
+        for(int i=0;i<(int)coefficient.size();i+=2)
+        {
+            if(i!=(int)coefficient.size()-2)
+            {
+                std::cout<<coefficient[i]*coefficient[i+1]<<' ';
+            }
+            else
+            {
+                std::cout<<coefficient[i]*coefficient[i+1];
+            }
+        }
     }
 };
 int main()
