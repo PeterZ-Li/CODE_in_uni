a49b20b9bc17884d55988c5d07deac42b98bb0eb
PeterZ Li <1846136440@qq.com>
2021-05-11 15:50:03 +0800

    version1, wrong answer

--- CODE_Cpp/Cpp_SINGLE/algorithm/PTA/PAT (Basic Level) Practice/1012_DataClassify.cpp
+++ CODE_Cpp/Cpp_SINGLE/algorithm/PTA/PAT (Basic Level) Practice/1012_DataClassify.cpp
@@ -3,7 +3,7 @@
 class Classification
 {
     private:
-    int N,n;//classify n one by one
+    int N,n,total;//classify n one by one
     struct sort//use a struct to define A
     {
         int A1,A2,A3,A5;
@@ -12,14 +12,106 @@ class Classification
     public:
     Classification(int N):N(N)//need to classify n one by one, use classify()
     {
-
+        while(N--)
+        {
+            std::cin>>n;
+            classify();
+        }
     }
     void classify()//classify a number from keyboard
     {
+        int temp,count=1;
+        temp=n%5;
+        switch (temp)
+        {
+        case 0:
+            {
+                if(n/2==0)
+                {
+                    A.A1+=n;
+                }
+            }
+            break;
+        case 1:
+            {
+                if(count)
+                {
+                    A.A2+=n;
+                    count=0;
+                }
+                else
+                {
+                    A.A2-=n;
+                    count=1;
+                }
+            }
+            break;
+        case 2:
+            {
+                A.A3++;
+            }
+            break;
+        case 4:
+            {
+                total++;
+                A.A4+=n;
+            }
+            break;
+        case 5:
+            {
+                if(n>A.A5)
+                {
+                    A.A5=n;
+                }
+            }
+            break;
+        default:
+            break;
+        }
         return;
     }
     void show()//show the struct A
     {
+        if(A.A1)
+        {
+            std::cout<<A.A1<<" ";
+        }
+        else
+        {
+            std::cout<<"N"<<" ";
+        }
+        if(A.A2)
+        {
+            std::cout<<A.A2<<" ";
+        }
+        else
+        {
+            std::cout<<"N"<<" ";
+        }
+        if(A.A3)
+        {
+            std::cout<<A.A3<<" ";
+        }
+        else
+        {
+            std::cout<<"N"<<" ";
+        }
+        if(A.A4)
+        {
+            std::cout<<"%.1d"<<A.A4/total;
+        }
+        else
+        {
+            std::cout<<"N"<<" ";
+        }
+        if(A.A5)
+        {
+            std::cout<<A.A5;
+        }
+        else
+        {
+            std::cout<<"N";
+        }
         return;
     }
 };
