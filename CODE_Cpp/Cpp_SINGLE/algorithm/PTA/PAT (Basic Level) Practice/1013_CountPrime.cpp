8497e26ca7783c15d4fb4d7818153a6cfa99df3e
PeterZ Li <1846136440@qq.com>
2021-05-11 20:47:22 +0800

    version1, overtime and some answers are wrong

--- CODE_Cpp/Cpp_SINGLE/algorithm/PTA/PAT (Basic Level) Practice/1013_CountPrime.cpp
+++ CODE_Cpp/Cpp_SINGLE/algorithm/PTA/PAT (Basic Level) Practice/1013_CountPrime.cpp
@@ -3,21 +3,61 @@
 class Nature
 {
     private:
-    int low,high,num=2;
+    int low,high,num=2,scount=0,count=1;
     public:
     Nature(int m,int n):low(m),high(n)
     {}
     void countprime()//count and control the whole progress
     {
-
+        if(low==high&&low==1)
+        {
+            std::cout<<2;
+        }
+        while(count<=high)
+        {
+            while(count>=low-1&&count<high)
+            {
+                if(isprime())
+                {
+                    count++;//count connect with prime, but change later than prime
+                    show();
+                }
+            }
+            if(isprime())
+            {
+                count++;
+            }
+        }
     }
     bool isprime()//judge if it is prime
     {
-
+        num++;
+        for(int i=2;i<num;i++)
+        {
+            if(num%i==0)
+            {
+                return false;
+            }
+        }
+        return true;
     }
     void show()//output the prime by the rule
     {
-
+        std::cout<<num;
+        if(count==high)
+        {
+            return;
+        }
+        scount++;
+        if(scount==10)
+        {
+            std::cout<<std::endl;
+            scount=0;
+        }
+        else
+        {
+            std::cout<<" ";
+        }
     }
 };
 int main()
