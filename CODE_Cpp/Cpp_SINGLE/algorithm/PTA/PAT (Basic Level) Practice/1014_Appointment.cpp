12b33b464afbebba13b647e3b8f773da1fc5a443
PeterZ Li <1846136440@qq.com>
2021-05-18 21:16:52 +0800

    finish define

---
+++ CODE_Cpp/Cpp_SINGLE/algorithm/PTA/PAT (Basic Level) Practice/1014_Appointment.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+
+#define N 4
+/*struct Time
+{
+    std::string day;
+    std::string hour;
+};there is no need to use structure. can use some algorithm to let character to num */
+//std::string day("MON","TUE","WED","THU","FRI","SAT","SUN");
+
+int main()
+{
+    const char DAY[][N]={"MON","TUE","WED","THU","FRI","SAT","SUN"};
+    std::string code[N];
+    int day,hour,minu,i;
+    return 0;
+}
