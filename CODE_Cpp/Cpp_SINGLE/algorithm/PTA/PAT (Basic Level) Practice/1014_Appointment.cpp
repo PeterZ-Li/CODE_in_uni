8e5d4d42be65ea1a12a94704ccab5ecc4d948e68
PeterZ Li <1846136440@qq.com>
2021-05-18 21:38:08 +0800

    version1, hour and minute are wrong

--- CODE_Cpp/Cpp_SINGLE/algorithm/PTA/PAT (Basic Level) Practice/1014_Appointment.cpp
+++ CODE_Cpp/Cpp_SINGLE/algorithm/PTA/PAT (Basic Level) Practice/1014_Appointment.cpp
@@ -13,6 +13,57 @@ int main()
 {
     const char DAY[][N]={"MON","TUE","WED","THU","FRI","SAT","SUN"};
     std::string code[N];
-    int day,hour,minu,i;
+    int i,counter=0;
+    for(int j=0;j<N;j++)
+    {
+        std::getline(std::cin,code[j]);
+    }
+    for(i=0;code[1][i]!='\0'||code[0][i]!='\0';i++)
+    {
+        if(counter==0)
+        {
+            if(code[0][i]>='A'&&code[0][i]<='G'&&code[0][i]==code[1][i])
+            {
+                counter=1;
+                std::cout<<DAY[code[0][i]-'A']<<" ";
+                continue;
+            }
+        }
+        else
+        {
+            if(code[0][i]==code[1][i])
+            {
+                if(code[0][i]>='0'&&code[0][i]<='9')
+                {
+                    std::cout<<code[0][i]-'0'+1<<":";
+                    break;
+                }
+                else if(code[0][i]>='a'&&code[0][i]<='n')
+                {
+                    std::cout<<code[0][i]-'a'+10<<":";
+                    break;
+                }
+                else if(code[0][i]>='A'&&code[0][i]<='N')
+                {
+                    std::cout<<code[0][i]-'a'+10<<":";
+                    break;
+                }
+            }
+        }
+    }
+    for(i=0;code[2][i]!='\0'||code[3][i]!='\0';i++)
+    {
+        if(code[2][i]==code[3][i])
+        {
+            if(i<=9)
+            {
+                std::cout<<0<<i;
+            }
+            else
+            {
+                std::cout<<i;
+            }
+        }
+    }
     return 0;
 }
