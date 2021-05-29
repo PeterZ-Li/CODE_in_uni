022b0d6b51c7b8b07df92c10875a05e95cf9ed6f
PeterZ Li <1846136440@qq.com>
2021-05-29 15:44:36 +0800

    only divide A into one, but if there is 0, this algorithm can not solve it perfectly

--- CODE_Cpp/Cpp_SINGLE/algorithm/PTA/PAT (Basic Level) Practice/1017_ADividedByB.cpp
+++ CODE_Cpp/Cpp_SINGLE/algorithm/PTA/PAT (Basic Level) Practice/1017_ADividedByB.cpp
@@ -4,22 +4,20 @@
 int main()
 {
     std::string A;
-    int B,divisor,dividend,outcome,remainder;
+    int B,divisor,outcome,remainder=0;
     std::cin>>A>>B;
     for(int i=0;i<A.length();i++)
     {
-        if(A[i]-'0'>=B)
+        divisor=remainder*10+(A[i]-'0');
+        outcome=divisor/B;
+        remainder=divisor%B;
+        if(outcome==0)
         {
-            outcome=(A[i]-'0')/B;
-            std::cout<<outcome;
-            remainder=(A[i]-'0')%B;
+            continue;
         }
         else
         {
-            divisor=(A[i]-'0')*10+(A[i+1]-'0');
-            i++;
-            std::cout<<divisor/B;
-            remainder=divisor%B;
+            std::cout<<outcome;
         }
     }
 }
