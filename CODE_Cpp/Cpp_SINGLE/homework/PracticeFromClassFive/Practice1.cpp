c6932979d765394269bd8c17a827477fee426708
PeterZ Li <1846136440@qq.com>
2021-05-31 21:18:28 +0800

    version1, input have sth wrong

---
+++ CODE_Cpp/Cpp_SINGLE/homework/PracticeFromClassFive/Practice1.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+
+class Assistent
+{
+    public:
+    Assistent()
+    {}
+    void AskWeather()
+    {
+        std::cout<<"Is it raining?"<<" (You can input Y/N)"<<std::endl;
+        char judge;
+        while(!(std::cin>>judge))
+        {
+            std::cout<<"Your input is wrong, please input again"<<std::endl;
+            std::cin.clear();
+            while(std::cin.get()!='\n')
+            {
+                continue;
+            }
+        }
+        if(judge=='Y')
+        {
+            std::cout<<"It is raining now!"<<std::endl;
+        }
+        else if(judge=='N')
+        {
+            std::cout<<"It is not raining now!"<<std::endl;
+        }
+        else
+        {
+            std::cout<<"Your input is wrong, please input again"<<std::endl;
+        }
+    }
+    void AskMark()
+    {
+        int mark;
+        std::cout<<"What's your test mark?"<<std::endl;
+        std::cin>>mark;
+        while(!(std::cin>>mark))
+        {
+            std::cout<<"Your input is wrong, please input again"<<std::endl;
+            std::cin.clear();
+            while(std::cin.get()!='\n')
+            {
+                continue;
+            }
+        }
+        if(mark>100||mark<0)
+        {
+            std::cout<<"Your input is wrong, please input again"<<std::endl;
+        }
+        switch((int)mark/10)
+        {
+            case 10:
+            case 9:std::cout<<"A"<<std::endl;break;
+		    case 8:std::cout<<"B"<<std::endl;break;
+		    case 7:
+		    case 6:std::cout<<"C"<<std::endl;break;
+		    default:std::cout<<"D"<<std::endl;break;
+        }
+    }
+};
+int main()
+{
+    Assistent cortana;
+    cortana.AskWeather();
+    cortana.AskMark();
+    system("pause");
+    return 0;
+}
