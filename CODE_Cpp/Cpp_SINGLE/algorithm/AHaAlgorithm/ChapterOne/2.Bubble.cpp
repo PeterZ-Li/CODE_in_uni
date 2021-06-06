1df39f006772b5d12084af1f9602697221f6cc60
PeterZ Li <1846136440@qq.com>
2021-06-06 14:39:44 +0800

    the last two elements have sth wrong in sequencing

---
+++ CODE_Cpp/Cpp_SINGLE/algorithm/AHaAlgorithm/ChapterOne/2.Bubble.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+
+int main()
+{
+    int n,num;
+    std::vector<int> NumList;
+    std::cin>>n;
+    while(n--)
+    {
+        std::cin>>num;
+        NumList.push_back(num);
+    }
+    n=NumList.size();
+    for(int i=0;i<n-2;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(NumList[j]<NumList[j+1])
+            {
+                std::swap(NumList[j],NumList[j+1]);
+            }
+        }
+    }
+    std::vector<int>::iterator it=NumList.begin();
+    for(;it!=NumList.end();it++)
+    {
+        std::cout<<*it<<" ";
+    }
+    return 0;
+}
