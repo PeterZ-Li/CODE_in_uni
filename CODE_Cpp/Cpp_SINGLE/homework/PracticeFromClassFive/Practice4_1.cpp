60066be53ac15d2621b1f926c95a49fb81215f55
PeterZ Li <1846136440@qq.com>
2021-06-13 15:52:48 +0800

    finish

---
+++ CODE_Cpp/Cpp_SINGLE/homework/PracticeFromClassFive/Practice4_1.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+class CEmployee
+{
+//    private://derived class also can not use it
+//    int a;
+    protected:
+    float m_salary;
+    public:
+    CEmployee()
+    {
+        m_salary=2000;
+    }
+    virtual ~CEmployee()=default;
+    virtual void Salary()
+    {
+    //    this->m_salary=2000;
+    }
+};
+
+class CSalesman:public CEmployee
+{
+    private:
+    float sales;
+    public:
+    CSalesman(float s):sales(s)
+    {}
+    void Salary()
+    {
+        m_salary=m_salary+sales*0.05;
+        std::cout<<m_salary<<std::endl;
+    }
+};
+
+class CDeskman:public CEmployee
+{
+    private:
+    int time;
+    public:
+    CDeskman(int t):time(t)
+    {}
+    void Salary()
+    {
+        if(time>=8)
+        {
+            m_salary+=100;
+        }
+        std::cout<<m_salary<<std::endl;
+    }
+};
+
+class CBuyer:public CEmployee
+{
+    private:
+    int cost;
+    public:
+    CBuyer(int c):cost(c)
+    {}
+    void Salary()
+    {
+        if(cost/100)
+        {
+            m_salary=(1+0.1*(cost/100))*m_salary;
+        }
+        std::cout<<m_salary<<std::endl;
+    }
+};
+
+int main()
+{
+    CSalesman cs(1000);
+    cs.Salary();
+    CDeskman cd(9);
+    cd.Salary();
+    CBuyer cb(300);
+    cb.Salary();
+    return 0;
+}
