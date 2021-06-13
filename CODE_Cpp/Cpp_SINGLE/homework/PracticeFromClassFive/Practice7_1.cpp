2047464ab440d4db974749cbeba23499d28d8402
PeterZ Li <1846136440@qq.com>
2021-06-13 17:36:31 +0800

    finish

---
+++ CODE_Cpp/Cpp_SINGLE/homework/PracticeFromClassFive/Practice7_1.cpp
@@ -0,0 +1,24 @@
+#include<fstream>
+#include<iostream>
+#include<string>
+
+int main()
+{
+    std::ifstream in;
+    in.open("Practice7_1.txt",std::ios::in);
+    if(in)
+    {
+        std::string fstr;
+        in>>fstr;
+        int num;
+        for(int i=0;i<(int)fstr.length();i++)
+        {
+            if((fstr[i]>='a'&&fstr[i]<='z')||(fstr[i]>='A'&&fstr[i]<='Z'))
+            {
+                num++;
+            }
+        }
+        std::cout<<num<<std::endl;
+    }
+    return 0;
+}
